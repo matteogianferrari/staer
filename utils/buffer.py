89807f3c8c83c9f16d92f883e18cec685808b462
matteogianferrari <mine.matt88@gmail.com>
2025-09-23 23:00:51 +0200

    Bug fix.

--- utils/buffer.py
+++ utils/buffer.py
@@ -460,7 +460,7 @@ class Buffer:
         Note:
             Only the examples are required. The other tensors are initialized only if they are provided.
         """
-        if logits and logits.dim() == 3:
+        if logits is not None and logits.dim() == 3:
             logits = logits.transpose(0, 1).contiguous()
 
         if not hasattr(self, 'examples'):
